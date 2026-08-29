4acd1ec5b774595a456560e4c38ed9fdd2c6f163
ziyang <167066912+ziyangliu-research@users.noreply.github.com>
2026-08-29 13:41:43 +0900

    Route TartanAir monocular datasets

--- utils/dataset_router.py
+++ utils/dataset_router.py
@@ -1,8 +1,11 @@
 from utils.dataset import load_dataset as load_builtin_dataset
-from utils.dataset_tartanair import TartanAirStereoDataset
+from utils.dataset_tartanair import TartanAirMonocularDataset, TartanAirStereoDataset
 
 
 def load_dataset(args, path, config):
-    if config["Dataset"]["type"] == "tartanair_stereo":
+    dataset_type = config["Dataset"]["type"]
+    if dataset_type == "tartanair_stereo":
         return TartanAirStereoDataset(args, path, config)
+    if dataset_type == "tartanair_mono":
+        return TartanAirMonocularDataset(args, path, config)
     return load_builtin_dataset(args, path, config)
