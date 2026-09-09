d71eb9ce02683cd932fc4f4d1d12dbf9b68e670e
ziyang <167066912+ziyangliu-research@users.noreply.github.com>
2026-09-09 20:56:10 +0900

    Route rectified ETH3D stereo and mono datasets

--- utils/dataset_router.py
+++ utils/dataset_router.py
@@ -1,4 +1,5 @@
 from utils.dataset import load_dataset as load_builtin_dataset
+from utils.dataset_eth3d import ETH3DMonocularDataset, ETH3DStereoDataset
 from utils.dataset_tartanair import TartanAirMonocularDataset, TartanAirStereoDataset
 
 
@@ -8,4 +9,8 @@ def load_dataset(args, path, config):
         return TartanAirStereoDataset(args, path, config)
     if dataset_type == "tartanair_mono":
         return TartanAirMonocularDataset(args, path, config)
+    if dataset_type == "eth3d_stereo":
+        return ETH3DStereoDataset(args, path, config)
+    if dataset_type == "eth3d_mono":
+        return ETH3DMonocularDataset(args, path, config)
     return load_builtin_dataset(args, path, config)
